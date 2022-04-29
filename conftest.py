6de203c598a090e06d0de53219dd63e278b9cd02
Your Name <you@example.com>
2022-04-29 19:07:07 +0300

    add conftest and test_items

---
+++ conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store',default='ru',
+                     help="Choose browser: chrome or firefox")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome(options=options)
+    browser.implicitly_wait(5)
+    yield browser
+    time.sleep(3)
+    print("\nquit browser..")
+    browser.quit()
---
+++ test_items.py
@@ -0,0 +1,13 @@
+import pytest
+from selenium.webdriver.common.by import By
+
+link_dict = [
+    'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/',
+]
+
+@pytest.mark.parametrize('linkac', link_dict)
+def test_shopping_cart_button(browser, linkac):
+    link = linkac
+    browser.get(link)
+    bt = browser.find_elements(By.CSS_SELECTOR, '.btn.btn-lg.btn-primary.btn-add-to-basket')
+    assert len(bt) > 0, 'button not found'
